61a4db64852fca7b146f1a19ec0031e8c3249768
yaelvinker <yaelvi116@gmail.com>
2019-10-27 15:54:54 +0200

    tmqi

--- TMQI.py
+++ TMQI.py
@@ -7,6 +7,7 @@ from scipy.signal import convolve
 from skimage.util import view_as_blocks
 from scipy.stats import norm, beta
 import hdr_image_utils
+import cv2
 
 def _RGBtoY(RGB):
     M = np.asarray([[0.2126, 0.7152, 0.0722], ])
@@ -122,7 +123,7 @@ def _StructuralFidelity(L_hdr, L_ldr, level, weight, window):
     return S, s_local, s_maps
 
 def print_result(Q, S, N, s_local, s_maps):
-    from scipy.misc import imsave
+    # from scipy.misc import imsave
     # for idx, sm in enumerate(s_maps):
     #     filename = "%s%i.%s" % ("s_map_", idx + 1, "float32")
     #
@@ -133,22 +134,8 @@ def print_result(Q, S, N, s_local, s_maps):
     #         imsave(filename, sm)
     print("Q = ", Q, " S = ", S, " N = ", N, "s_local = ", s_local)
 
-# def TMQI(ldr, hdr):
-#
 
-
-if __name__ == '__main__':
-    im_hdr = imageio.imread("data/hdr_data/hdr_data/S0010.hdr").astype('float32')
-    L_hdr = skimage.transform.resize(im_hdr, (256, 256),  mode='reflect', preserve_range=False)
-    hdr_image_utils.print_image_details(L_hdr, "L_hdr AFTER READ")
-
-    data = np.load("data/hdr_log_data/hdr_log_data/S0010_1000.npy", allow_pickle=True)
-    L_ldr = data[()]["input_image"]
-    L_ldr = np.squeeze(L_ldr.clone().permute(1, 2, 0).detach().cpu().numpy())
-    L_ldr = to_0_1_range(L_ldr)
-    hdr_image_utils.print_image_details(L_ldr, "L_ldr AFTER READ")
-    L_hdr = L_ldr
-
+def TMQI(L_hdr, L_ldr):
     if len(L_hdr.shape) == 3:
         # Processing RGB images
         L_hdr = _RGBtoY(L_hdr)
@@ -156,14 +143,14 @@ if __name__ == '__main__':
     if len(L_ldr.shape) == 3:
         L_ldr = _RGBtoY(L_ldr)
 
-    hdr_image_utils.print_image_details(L_hdr, "L_hdr AFTER GRAY SCALE")
+    # hdr_image_utils.print_image_details(L_hdr, "L_hdr AFTER GRAY SCALE")
 
     a = 0.8012
     Alpha = 0.3046
     Beta = 0.7088
     lvl = 5  # levels
     weight = [0.0448, 0.2856, 0.3001, 0.2363, 0.1333]
-    M, N = L_hdr.shape
+    # M, N = L_hdr.shape
     window = None
 
     if window is None:
@@ -174,17 +161,17 @@ if __name__ == '__main__':
     N = StatisticalNaturalness(L_ldr)
 
     # The images should have the same dynamic ranges, e.g. [0,255]
-    factor = float(2**8 - 1.)
+    factor = float(2 ** 8 - 1.)
 
-    hdr_image_utils.print_image_details(L_ldr, "L_ldr before RE-FACTOR")
-    hdr_image_utils.print_image_details(L_hdr, "L_hdr before RE-FACTOR")
+    # hdr_image_utils.print_image_details(L_ldr, "L_ldr before RE-FACTOR")
+    # hdr_image_utils.print_image_details(L_hdr, "L_hdr before RE-FACTOR")
 
     # if self.original:
     L_hdr = factor * (L_hdr - L_hdr.min()) / (L_hdr.max() - L_hdr.min())
     L_ldr = factor * (L_ldr - L_ldr.min()) / (L_ldr.max() - L_ldr.min())
 
-    hdr_image_utils.print_image_details(L_ldr, "L_ldr AFTER RE-FACTOR")
-    hdr_image_utils.print_image_details(L_hdr, "L_hdr AFTER RE-FACTOR")
+    # hdr_image_utils.print_image_details(L_ldr, "L_ldr AFTER RE-FACTOR")
+    # hdr_image_utils.print_image_details(L_hdr, "L_hdr AFTER RE-FACTOR")
 
     S, s_local, s_maps = _StructuralFidelity(L_hdr, L_ldr, lvl, weight, window)
     Q = a * (S ** Alpha) + (1. - a) * (N ** Beta)
@@ -195,4 +182,67 @@ if __name__ == '__main__':
     #     L_hdr = factor * (L_hdr - L_hdr.min()) / (L_hdr.max() - L_hdr.min())
     #     L_ldr = factor * (L_ldr - L_ldr.min()) / (L_ldr.max() - L_ldr.min())
     #
+
+def hdr_log_loader_factorize(im_origin, range_factor):
+    max_origin = np.max(im_origin)
+    image_new_range = (im_origin / max_origin) * range_factor
+    im_log = np.log(image_new_range + 1)
+    im = (im_log / np.log(range_factor + 1)).astype('float32')
+    return im
+
+def log_tone_map(path):
+    return hdr_log_loader_factorize(path, 100)
+
+def Dargo_tone_map(im):
+
+    # Tonemap using Drago's method to obtain 24-bit color image
+    im = im / np.max(im)
+    tonemapDrago = cv2.createTonemapDrago(1.0, 0.7, 0.85)
+    ldrDrago = tonemapDrago.process(im)
+    # ldrDrago = 3 * ldrDrago
+    hdr_image_utils.print_image_details(ldrDrago,"dargo")
+
+def Durand_tone_map(im):
+    # Tonemap using Durand's method obtain 24-bit color image
+    tonemapDurand = cv2.createTonemapDurand(1.5,4,1.0,1,1)
+    ldrDurand = tonemapDurand.process(im)
+    # ldrDurand = 3 * ldrDurand
+    hdr_image_utils.print_image_details(ldrDurand,"durand")
+
+if __name__ == '__main__':
+    hdr_path = "data/hdr_data/hdr_data/S0010.hdr"
+    im_hdr = imageio.imread(hdr_path).astype('float32')
+    tone_map_im = im_hdr
+    # hdr_image_utils.print_image_details(_L_hdr, "L_hdr AFTER READ")
+    _L_hdr = skimage.transform.resize(im_hdr, (256, 256), mode='reflect', preserve_range=False)
+
+    tone_map_methods = ["log_100", "Dargo", "None"]
+    for t_m in tone_map_methods:
+        if t_m == "None":
+            print()
+        if t_m == "log_100":
+            print("LOG TONE MAP RESULTS : ")
+            _L_ldr = log_tone_map(im_hdr)
+        if t_m == "Dargo":
+            print("DARGO: ")
+            _L_ldr = Dargo_tone_map(im_hdr)
+
+
+    print("NON TONE MAP RESULTS : ")
+    TMQI(_L_hdr, _L_hdr)
+
+    print("LOG TONE MAP RESULTS : ")
+    log_tone_map_im = log_tone_map(_L_hdr)
+    TMQI(_L_hdr, log_tone_map_im)
+    Dargo_tone_map(im_hdr)
+    Durand_tone_map(im_hdr)
+
+    data = np.load("data/hdr_log_data/hdr_log_data/S0010_1000.npy", allow_pickle=True)
+    L_ldr = data[()]["input_image"]
+    L_ldr = np.squeeze(L_ldr.clone().permute(1, 2, 0).detach().cpu().numpy())
+    _L_ldr = to_0_1_range(L_ldr)
+    # hdr_image_utils.print_image_details(L_ldr, "L_ldr AFTER READ")
+    _L_hdr = _L_ldr
+
+    TMQI(_L_hdr, _L_ldr)
 
